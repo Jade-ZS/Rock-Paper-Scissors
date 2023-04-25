747f1403ed07e2f1013deea5f7507ad322461bbb
Jade Shi <ziyushi1997@gmail.com>
2023-04-24 18:07:01 -0700

    Refactor JS files and related html

--- main.js
+++ main.js
@@ -34,117 +34,63 @@ var endGameButton = document.querySelector('.end-game-button');
 
 // event listeneres
 window.addEventListener('load', () => {
-  if (localStorage.length > 3) {
-    var humanAvatar = localStorage.getItem('avatar');
-    var humanName = localStorage.getItem('name');
-    var humanWins = parseInt(localStorage.getItem('humanWins'));
-    var computerWins = parseInt(localStorage.getItem('computerWins'));
-    currentGame = createGame(humanName, humanAvatar, humanWins, computerWins);
+  if (localStorage.length > 0) {
+    storageToData()
     displayHome();
   } else {
-    displayLogIn();
     currentGame = createGame();
-  }
-});
-
-avatarField.addEventListener('change', ()=> {
-  if(avatarField.value === 'customize') {
-    ifShowItems([imgUploadSection], true);
-  } else {
-    ifShowItems([imgUploadSection], false);
+    displayLogIn();
   }
 });
 
 loginView.addEventListener('submit', event => {
   event.preventDefault();
-  currentGame.humanPlayer.name = nameField.value;
-
-  if(avatarField.value !== "customize") {
-    currentGame.humanPlayer.avatar = avatarField.value;
-    localStorage.setItem('avatar', avatarField.value);
-    localStorage.setItem('humanWins', '0');
-    localStorage.setItem('computerWins', '0');
-    localStorage.setItem('name', currentGame.humanPlayer.name);
-  } else {
-    currentGame.humanPlayer.avatar = imgURL;
-    localStorage.setItem('avatar', currentGame.humanPlayer.avatar);
-    localStorage.setItem('humanWins', '0');
-    localStorage.setItem('computerWins', '0');
-    localStorage.setItem('name', currentGame.humanPlayer.name);
-  }
+  getUserInfo();
+  dataToStorage();
   displayHome();
 });
 
-imgInput.addEventListener('change', () => {
-  var img = imgInput.files[0];
-  var reader = new FileReader();
-  reader.addEventListener('load', () => {
-     imgURL = reader.result;
-  });
-  reader.readAsDataURL(img);
-}); 
+avatarField.addEventListener('change', ()=> {
+  displayUploadSection();
+});
 
+imgInput.addEventListener('change', getUploadedImg); 
+
+asGuestButton.addEventListener('click', () => {
+  dataToStorage();
+  displayHome();
+});
 
 gameBoard.addEventListener('click', event => {
   if (event.target.classList.contains('mode') || event.target.parentElement.classList.contains('mode')) {
-    console.log('mode')
     getGameMode(event);
     displayFighters();
   } 
-
   if (event.target.nodeName === 'IMG' && !fighters.classList.contains('hidden')) {
-    console.log('click on a fighter')
     displayResult(event);
-    setTimeout(resetGameBoard, 1000);
+    setTimeout(resetGameBoard, 2000);
   }
-  
   if (event.target.classList.contains('return-home-button')) {
-    console.log('return home')
     displayHome();
   }
-
   if (event.target.classList.contains('change-mode-button')) {
-    console.log('change mode')
     showAlertMessage(`Your game mode will be changed!`);
   }
-
   if (event.target.classList.contains('cancel-button')) {
-    console.log('cancle change')
     ifShowItems([alertBox], false);
   }
-
   if (event.target.classList.contains('ok-button')) {
-    console.log('confirm change')
     toggleMode(currentGame.mode);
     ifShowItems([alertBox], false);
   }
-
 });
 
 endGameButton.addEventListener('click', () => {
-  console.log('end game')
   localStorage.clear();
+  currentGame = createGame();
   displayLogIn();
 });
 
-asGuestButton.addEventListener('click', () => {
-  console.log('as guest')
-  localStorage.setItem('avatar', currentGame.humanPlayer.avatar);
-  localStorage.setItem('humanWins', '0');
-  localStorage.setItem('computerWins', '0');
-  localStorage.setItem('name', currentGame.humanPlayer.name);
-  displayHome();
-});
-
-function toggleMode() {
-  console.log('toggleMode')
-  if (currentGame.mode === easyMode) {
-    currentGame.mode = difficultMode;
-  } else {
-    currentGame.mode = easyMode;
-  }
-  displayFighters();
-}
 // event handlers
 function createPlayer(name, avatar, wins) {
   var player = {
@@ -165,14 +111,63 @@ function createGame(name, avatar, humanWins, computerWins) {
   return game;
 }
 
-function generateRandomFighter(fighters) {
-  console.log('random fighter')
-  var index = Math.floor(fighters.length * Math.random());
-  return fighters[index];
+function storageToData() {
+  var humanAvatar = localStorage.getItem('avatar');
+  var humanName = localStorage.getItem('name');
+  var humanWins = parseInt(localStorage.getItem('humanWins'));
+  var computerWins = parseInt(localStorage.getItem('computerWins'));
+  currentGame = createGame(humanName, humanAvatar, humanWins, computerWins);
+}
+
+function dataToStorage() {
+  localStorage.setItem('avatar', currentGame.humanPlayer.avatar);
+  localStorage.setItem('humanWins', currentGame.humanPlayer.wins);
+  localStorage.setItem('computerWins', currentGame.computerPlayer.wins);
+  localStorage.setItem('name', currentGame.humanPlayer.name);
+}
+
+function displayLogIn() {
+  var itemsToShow= [loginView];
+  ifShowItems(itemsToShow, true);
+  var itemsToHide = [gameView];
+  ifShowItems(itemsToHide, false);
+}
+
+function getUserInfo() {
+  currentGame.humanPlayer.name = nameField.value;
+  if(avatarField.value !== "customize") {
+    currentGame.humanPlayer.avatar = avatarField.value;
+  } else {
+    currentGame.humanPlayer.avatar = imgURL;
+  }
+}
+
+function displayUploadSection() {
+  if(avatarField.value === 'customize') {
+    ifShowItems([imgUploadSection], true);
+  } else {
+    ifShowItems([imgUploadSection], false);
+  }
+}
+
+function getUploadedImg() {
+  var img = imgInput.files[0];
+  var reader = new FileReader();
+  reader.addEventListener('load', () => {
+     imgURL = reader.result;
+  });
+  reader.readAsDataURL(img);
+}
+
+function displayHome() {
+  renderSideBar(players);
+  var itemsToShow= [gameView].concat(Array.from(modes));
+  ifShowItems(itemsToShow, true);
+  var itemsToHide = [result, fighters, loginView, changeModeButton, returnHomeButton];
+  ifShowItems(itemsToHide, false);
 }
 
 function getGameMode(event) {
-  console.log('get game mode')
   var choiceBox = event.target.parentElement.firstElementChild;
   if (event.target.innerText.indexOf('CLASSIC') !== -1|| choiceBox.innerText.indexOf("CLASSIC") !== -1) {
     currentGame.mode = easyMode; 
@@ -184,6 +179,37 @@ function getGameMode(event) {
   return currentGame.mode;
 }
 
+function toggleMode() {
+  if (currentGame.mode === easyMode) {
+    currentGame.mode = difficultMode;
+  } else {
+    currentGame.mode = easyMode;
+  }
+  displayFighters();
+}
+
+function displayFighters() {
+  showMessage('Choose your fighter!');
+  renderFighters(currentGame.mode);
+  var itemsToShow = [fighters, changeModeButton, returnHomeButton];
+  ifShowItems(itemsToShow, true);
+  var itemsToHide = [loginView, result].concat(Array.from(modes));
+  ifShowItems(itemsToHide, false);
+}
+
+function resetGameBoard() {
+  var itemsToShow= [fighters];
+  ifShowItems(itemsToShow, true);
+  var itemsToHide = [result];
+  ifShowItems(itemsToHide, false);
+  showMessage('Choose your fighters!');
+}
+
+function generateRandomFighter(fighters) {
+  var index = Math.floor(fighters.length * Math.random());
+  return fighters[index];
+}
+
 function getUserFighter(event) {
   currentGame.humanPlayer.currentChoice = event.target;
     for (var i = 0; i < currentGame.mode.length; i++) {
@@ -193,8 +219,29 @@ function getUserFighter(event) {
     }
 }
 
+function displayResult(event) {
+  var itemsToShow= [result];
+  ifShowItems(itemsToShow, true);
+  var itemsToHide = [fighters];
+  ifShowItems(itemsToHide, false);
+  getUserFighter(event);
+  renderResult();
+  showResultText();  
+  renderSideBar(players);
+}
+
+function showResultText() {
+  var winner = determineWinner(currentGame.humanPlayer, currentGame.computerPlayer);
+  var message;
+  if (winner) {
+    message = `${winner.name} won!`; 
+  } else {
+    message = `It's a draw!`;
+  }
+  showMessage(message);
+}
+
 function determineWinner(player1, player2) {
-  console.log('determine winner')
   var winner;
   var condition1 = player1.currentChoice.fighter === 'rock' && player2.currentChoice.fighter === ('scissors'||'lizard');
   var condition2 = player1.currentChoice.fighter === 'paper' && player2.currentChoice.fighter === ('rock'||'alien');
@@ -208,81 +255,19 @@ function determineWinner(player1, player2) {
   } else {
     winner = player2; 
   }
-  winner.wins+=1;
-  console.log('human: ', currentGame.humanPlayer.wins)
-  console.log('computer: ', currentGame.computerPlayer.wins)
-  console.log('--------')
+  
+  winner.wins += 1;
+  dataToStorage();
+ 
   return winner;
 }
 
-function uploadImage() {
-  ifShowItems([imgUploadSection], true);
-}
 
-function showResultText() {
-  console.log('show result text')
-  var winner = determineWinner(currentGame.humanPlayer, currentGame.computerPlayer);
-  if (winner.name === localStorage.getItem('name')) {
-    localStorage.setItem('humanWins', winner.wins)
-  } else {
-    localStorage.setItem('computerWins', winner.wins);
-  }
 
-  var message;
-  if (winner) {
-    message = `${winner.name} won!`; 
-  } else {
-    message = `It's a draw!`;
-  }
-  showMessage(message);
-}
 
-function displayLogIn() {
-  console.log('display log in')
-  var itemsToShow= [loginView];
-  ifShowItems(itemsToShow, true);
-  var itemsToHide = [gameView];
-  ifShowItems(itemsToHide, false);
-}
 
-function displayHome() {
-  console.log('display home')
-  renderPlayers(players);
-  var itemsToShow= [gameView].concat(Array.from(modes));
-  ifShowItems(itemsToShow, true);
-  var itemsToHide = [result, fighters, loginView, changeModeButton, returnHomeButton];
-  ifShowItems(itemsToHide, false);
-}
 
-function resetGameBoard() {
-  console.log('reset game board')
-  var itemsToShow= [fighters];
-  ifShowItems(itemsToShow, true);
-  var itemsToHide = [result];
-  ifShowItems(itemsToHide, false);
-  showMessage('Choose your fighters!');
-}
 
-function displayFighters() {
-  console.log('display fighters')
-  showMessage('Choose your fighter!');
-  renderFighters(currentGame.mode);
-  var itemsToShow = [fighters, changeModeButton, returnHomeButton];
-  ifShowItems(itemsToShow, true);
-  var itemsToHide = [loginView, result].concat(Array.from(modes));
-  ifShowItems(itemsToHide, false);
-}
 
-function displayResult(event) {
-  console.log('display result')
-  var itemsToShow= [result];
-  ifShowItems(itemsToShow, true);
-  var itemsToHide = [fighters];
-  ifShowItems(itemsToHide, false);
-  getUserFighter(event);
-  renderResult();
-  showResultText();  
-  renderPlayers(players);
-}
 
 
--- updateDom.js
+++ updateDom.js
@@ -12,20 +12,22 @@ function showMessage(message) {
   subline.innerText = message;
 }
 
+function showAlertMessage(message) {
+  alertMessage.innerText = message;
+  ifShowItems([alertBox], true);
+}
 
-function renderFighters(mode) {
-  console.log('render fighters')
+function renderFighters(choices) {
   fighters.innerHTML = '';
   for (var i = 0; i < 5; i++) {
-    if (!mode[i]) {
+    if (!choices[i]) {
       return;
     }
-    fighters.innerHTML += `<img src=${mode[i].img} alt=${mode[i].fighter}>`
+    fighters.innerHTML += `<img src=${choices[i].img} alt=${choices[i].fighter}>`
   }
 }
 
 function renderResult() {
-  console.log('render result')
   currentGame.computerPlayer.currentChoice = generateRandomFighter(currentGame.mode);
   result.innerHTML = `
     <img class="human-choice" src=${currentGame.humanPlayer.currentChoice.img} alt=${currentGame.humanPlayer.currentChoice.fighter}>
@@ -33,22 +35,15 @@ function renderResult() {
   `;
 }
 
-function renderPlayers(players) {
-  console.log('render players')
+function renderSideBar(players) {
   var leftChildren = players[0].children;
-  console.log('hererere')
   if (currentGame.humanPlayer.avatar.includes('data')) {
-    console.log(currentGame.humanPlayer.avatar.imgSrc)
-    // leftChildren[0].innerHTML = `<img src=${currentGame.humanPlayer.avatar.imgSrc}>`
-    console.log('render img')
     leftChildren[0].innerHTML = `<img src=${localStorage.getItem('avatar')}>`
   } else {
     leftChildren[0].innerText = localStorage.getItem('avatar');
   }
   leftChildren[1].innerText = localStorage.getItem('name');
-  console.log('localStorage.humanWins', localStorage.getItem('humanWins'))
   leftChildren[2].innerText = `Wins: ${localStorage.getItem('humanWins')}`;
-  console.log('localStorage', localStorage)
 
   var rightChildren = players[1].children;
   rightChildren[0].innerText = currentGame.computerPlayer.avatar;
@@ -56,7 +51,3 @@ function renderPlayers(players) {
   rightChildren[2].innerText = `Wins: ${localStorage.getItem('computerWins')}`;
 }
 
-function showAlertMessage(message) {
-  alertMessage.innerText = message;
-  ifShowItems([alertBox], true);
-}
